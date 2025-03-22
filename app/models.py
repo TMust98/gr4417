757b995d8e7dc1d5a838ede744319dcc1f62bf0c
unknown <timmust98@gmail.com>
2025-03-22 13:37:14 +0300

    flask3

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(30), unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+    def __repr__(self):
+        return f'<user {self.username}>'
+
